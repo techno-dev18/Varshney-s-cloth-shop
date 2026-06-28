b17c44dedf5f95e73a8f59bc128300c8eb39ad52
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:08:59 +0530

    added cloth data

---
+++ src/Data/clothData.js
@@ -0,0 +1,69 @@
+export const categories = [
+    {
+      categoryName: "Shirts",
+      imageURL:
+        "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf"
+    },
+    {
+      categoryName: "Jeans",
+      imageURL:
+        "https://images.unsplash.com/photo-1542272604-787c3835535d"
+    },
+    {
+      categoryName: "Jackets",
+      imageURL:
+        "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
+    }
+  ];
+  
+  export const products = [
+    {
+      productName: "Classic Black Shirt",
+      category: "Shirts",
+      gender: "male",
+      ratings: 4.5,
+      brand: "Varshney",
+      price: 2200,
+      discountPercentage: 20,
+      imgURL:
+        "https://images.unsplash.com/photo-1603252109303-2751441dd157",
+      sizes: ["S","M","L","XL"],
+      tagline: "Luxury Black Shirt",
+      description:
+        "Premium cotton black shirt.",
+      features: [
+        "100% Cotton",
+        "Regular Fit",
+        "Premium Fabric"
+      ],
+      details: [
+        "Wash Cold",
+        "Machine Wash",
+        "Made in India"
+      ]
+    },
+  
+    {
+      productName: "Golden Jacket",
+      category: "Jackets",
+      gender: "male",
+      ratings: 4.8,
+      brand: "Varshney",
+      price: 4500,
+      discountPercentage: 15,
+      imgURL:
+        "https://images.unsplash.com/photo-1556905055-8f358a7a47b2",
+      sizes: ["M","L","XL"],
+      tagline: "Luxury Jacket",
+      description:
+        "Elegant premium jacket.",
+      features: [
+        "Water Resistant",
+        "Premium Fabric"
+      ],
+      details: [
+        "Dry Clean",
+        "Winter Wear"
+      ]
+    }
+  ];
