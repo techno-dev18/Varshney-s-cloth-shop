494d97f39d8cc77630f7d415c65fed6221cf5221
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:00:36 +0530

    added basket and css

---
+++ src/Pages/Basket.jsx
@@ -0,0 +1,11 @@
+import "../Styles/Basket.css";
+
+const Basket = () => {
+  return (
+    <div className="basketPage">
+      <h1>Your Shopping Basket</h1>
+    </div>
+  );
+};
+
+export default Basket;
