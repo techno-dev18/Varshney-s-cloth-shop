7155171b8cd81a773d07b393152c2ff436071267
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:19:46 +0530

    added basket page

--- src/Pages/Basket.jsx
+++ src/Pages/Basket.jsx
@@ -1,11 +1,79 @@
-import "../Styles/Basket.css";
-
-const Basket = () => {
-  return (
-    <div className="basketPage">
-      <h1>Your Shopping Basket</h1>
-    </div>
-  );
-};
-
-export default Basket;
+import {
+    useSelector,
+    useDispatch
+  }
+  from "react-redux";
+  
+  import {
+    deleteProduct
+  }
+  from "../Redux/slices/basketSlice";
+  
+  import "../Styles/Basket.css";
+  
+  const Basket = () => {
+  
+    const items =
+      useSelector(
+        store =>
+          store.basket
+      );
+  
+    const dispatch =
+      useDispatch();
+  
+    return (
+  
+      <div className="basketPage">
+  
+        <h1>
+          Shopping Basket
+        </h1>
+  
+        {
+          items.length === 0 ?
+  
+          <h2>
+            Basket Empty
+          </h2>
+  
+          :
+  
+          items.map(item => (
+  
+            <div
+              className="basketCard"
+              key={item.productName}
+            >
+  
+              <img
+                src={item.imgURL}
+                alt=""
+              />
+  
+              <h3>
+                {item.productName}
+              </h3>
+  
+              <button
+                onClick={() =>
+                  dispatch(
+                    deleteProduct(
+                      item.productName
+                    )
+                  )
+                }
+              >
+                Remove
+              </button>
+  
+            </div>
+  
+          ))
+        }
+  
+      </div>
+    );
+  };
+  
+  export default Basket;
