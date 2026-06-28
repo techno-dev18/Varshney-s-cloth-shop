b37dabe7103389fc1c532308c5e60396c1358456
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:21:50 +0530

    added dashboard page and css

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,39 @@
 import { Link } from "react-router-dom";
 import FooterBar from "../Components/FooterBar";
+import {
+  categories
+}
+from "../Data/clothData";
 
+import CategoryTile
+from "../Components/CategoryTile";
 import "../Styles/Dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
+    <div className="categorySection">
+
+<h1>
+  Popular Categories
+</h1>
+
+<div className="categoryGrid">
+
+  {
+    categories.map(category => (
+
+      <CategoryTile
+        key={category.categoryName}
+        category={category}
+      />
+
+    ))
+  }
+
+</div>
+
+</div>
       <section className="hero">
         <img
           src="https://images.pexels.com/photos/33517294/pexels-photo-33517294.jpeg"
