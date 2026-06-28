29f473d417c142c9b339cda1acaeb7a14bce2763
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:14:50 +0530

    added basket slice

--- src/Redux/slices/basketSlice.js
+++ src/Redux/slices/basketSlice.js
@@ -1,9 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const basketSlice = createSlice({
-  name:"basket",
-  initialState:[],
-  reducers:{}
-});
-
-export default basketSlice.reducer;
+import {
+    createSlice
+  }
+  from "@reduxjs/toolkit";
+  
+  const basketSlice =
+  createSlice({
+  
+    name:"basket",
+  
+    initialState:[],
+  
+    reducers:{
+  
+      insertProduct:
+      (state, action) => {
+  
+        state.push(
+          action.payload
+        );
+      },
+  
+      deleteProduct:
+      (state, action) => {
+  
+        return state.filter(
+          item =>
+            item.productName !==
+            action.payload
+        );
+      }
+    }
+  });
+  
+  export const {
+    insertProduct,
+    deleteProduct
+  } = basketSlice.actions;
+  
+  export default basketSlice.reducer;
