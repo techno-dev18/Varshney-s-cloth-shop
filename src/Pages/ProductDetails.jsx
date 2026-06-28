a489edcd53bd56b3ac7d3e115af7f5054d8953a9
techno-dev18 <technologix2op@gmail.com>
2026-06-28 20:07:25 +0530

    added product details

---
+++ src/Pages/ProductDetails.jsx
@@ -0,0 +1,135 @@
+import { useParams } from "react-router-dom";
+
+import { products }
+from "../Data/clothData";
+
+import "../Styles/ProductDetails.css";
+
+const ProductDetails = () => {
+
+  const { itemName } =
+    useParams();
+
+  const selectedProduct =
+    products.find(
+      product =>
+        product.productName ===
+        itemName
+    );
+
+  if (!selectedProduct) {
+    return <h1>Product Not Found</h1>;
+  }
+
+  const finalPrice =
+    Math.round(
+      selectedProduct.price -
+      (
+        selectedProduct.price *
+        selectedProduct.discountPercentage
+      ) / 100
+    );
+
+  return (
+    <section className="detailsPage">
+
+      <div className="detailsContainer">
+
+        <div className="imageSide">
+
+          <img
+            src={selectedProduct.imgURL}
+            alt=""
+          />
+
+        </div>
+
+        <div className="infoSide">
+
+          <span className="category">
+            {selectedProduct.category}
+          </span>
+
+          <h1>
+            {selectedProduct.productName}
+          </h1>
+
+          <p>
+            {selectedProduct.tagline}
+          </p>
+
+          <h3>
+            ⭐ {selectedProduct.ratings}
+          </h3>
+
+          <div className="priceBox">
+
+            <span className="old">
+              ₹{selectedProduct.price}
+            </span>
+
+            <span className="new">
+              ₹{finalPrice}
+            </span>
+
+          </div>
+
+          <p>
+            {selectedProduct.description}
+          </p>
+
+          <select>
+
+            {
+              selectedProduct.sizes.map(
+                size => (
+                  <option key={size}>
+                    {size}
+                  </option>
+                )
+              )
+            }
+
+          </select>
+
+          <h3>Features</h3>
+
+          <ul>
+            {
+              selectedProduct.features.map(
+                feature => (
+                  <li key={feature}>
+                    {feature}
+                  </li>
+                )
+              )
+            }
+          </ul>
+
+          <h3>Details</h3>
+
+          <ul>
+            {
+              selectedProduct.details.map(
+                detail => (
+                  <li key={detail}>
+                    {detail}
+                  </li>
+                )
+              )
+            }
+          </ul>
+
+          <button>
+            Add To Basket
+          </button>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+};
+
+export default ProductDetails;
