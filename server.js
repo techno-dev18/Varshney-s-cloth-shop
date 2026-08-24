2da843b29526d047d07da6b6b79eced0a7f1fcae
techno-dev18 <technologix2op@gmail.com>
2026-08-24 20:23:19 +0530

    added few feature

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./db/dbconc.js";
 import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
