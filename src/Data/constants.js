859cb1061c5b02fbf347b6abd42cb054005bc3a4
techno-dev18 <technologix2op@gmail.com>
2026-09-03 19:09:02 +0530

    added feature

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -1,8 +1,8 @@
 export const heroImage =
   "/images/cs.png";
 
-  export const HeroImage = () => {
-  return (
-    <img src={heroImage} alt="Hero" />
-  );
-};
+//   export const HeroImage = () => {
+//   return (
+//     <img src={heroImage} alt="Hero" />
+//   );
+// };
